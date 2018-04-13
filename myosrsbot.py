8eb847c42bd70742fa55a8c57142e38330ed9b73
Christopher Kok <ckok@purdue.edu>
2018-04-13 17:42:23 -0400

    Bank loop complete
    - can walk back and forth from bank with ease

--- myosrsbot.py
+++ myosrsbot.py
@@ -27,10 +27,10 @@ def travel_time(x2, y2):
 
     return max(uniform(.08, .12), rate * (distance/randint(250, 270)))
 
-def bank_loop(bank_locations, triggers):
+def bank_loop(bank_locations):
     """Makes a trip to the bank to deposit the iron ore. Takes 16-17 seconds"""
     order = ['map1', 'map2', 'map3', 'map4', 'map5', 'map6', 'map7', 'deposit']
-    waits = [(14, 15), (14, 15), (12, 14), (12, 14), (12, 14), (12, 14), (4, 5), (4, 5)]
+    waits = [(14, 15), (14, 15), (12, 14), (12, 14), (12, 14), (10, 11), (2, 3), (4, 5)]
 
     for i in range(len(order)):
         random_coordinate(bank_locations[order[i]])
@@ -41,6 +41,13 @@ def bank_loop(bank_locations, triggers):
         random_wait(waits[i][0], waits[i][1])
         # random_wait(12, 14)
 
+    back_order = ['mapb1', 'mapb2', 'mapb3', 'mapb4', 'mapb5', 'mapb6']
+
+    for i in range(len(back_order)):
+        random_coordinate(bank_locations[back_order[i]])
+        pag.click()
+        random_wait(15, 17)
+
 def mine_loop(rock_locations, triggers, mininglap):
     # order = ['rock1', 'rock2']
     order = ['rock1']
@@ -48,8 +55,8 @@ def mine_loop(rock_locations, triggers, mininglap):
 
     for i in range(len(order)):
         # Checks for full inventory.
-        # if not self.image_match((2352, 682, 63, 55), 'triggers/bankslot.png'):
-        #    return True
+        if not image_match((863, 577, 45, 45), 'triggers/bagfull.png'):
+        	return True
 
         random_coordinate(rock_locations[order[i]])
         wait_for_trigger(triggers[trigger_order[(i*2)]])  # wait for iron
@@ -67,7 +74,13 @@ def mine_loop(rock_locations, triggers, mininglap):
     return
 
 def image_match(r, img):
-    pag.screenshot('triggers/screenie.png', region=r)
+    try:
+        pag.screenshot('triggers/screenie.png', region=r)
+    except OSError:
+        print("error with screenshot")
+        random_wait(0.2,0.5)
+        pag.screenshot('triggers/screenie.png', region=r)
+
     screen = cv2.imread('triggers/screenie.png')
     template = cv2.imread(img)
 
@@ -98,7 +111,10 @@ rock_locations = {'rock1': (300, 287, 35, 35)}
 bank_locations = {'map1': (855, 73, 0, 0), 'map2': (803, 52, 0, 0),
 					'map3': (749, 78, 0, 0), 'map4': (749, 78, 0, 0),
 					'map5': (718, 152, 0, 0), 'map6': (803, 174, 0, 0),
-					'map7': (319, 292, 0, 0), 'deposit': (540, 413, 13, 13),}
+					'map7': (319, 292, 0, 0), 'deposit': (540, 413, 13, 13),
+					'mapb1': (882, 104, 0, 0), 'mapb2': (871, 187, 0, 0),
+					'mapb3': (838, 203, 0, 0), 'mapb4': (808, 231, 0, 0),
+					'mapb5': (786, 226, 0, 0), 'mapb6': (774, 162, 0, 0)}
 
 # rock_triggers = {'rock1iron': (315, 285, 5, 5, 'triggers/tin1.png'),
 #                  'rock1noiron': (315, 285, 5, 5, 'triggers/notin1.png'),
@@ -106,12 +122,17 @@ bank_locations = {'map1': (855, 73, 0, 0), 'map2': (803, 52, 0, 0),
 #                  'rock2noiron': (262, 231, 5, 5, 'triggers/notin2.png')}
 rock_triggers = {'rock1iron': (300, 287, 35, 35, 'triggers/cop1.png'),
                  'rock1noiron': (300, 287, 35, 35, 'triggers/nocop1.png')}
-
-try: 
+lap = 0
+try:
 	while True:
-		mine_loop(rock_locations, rock_triggers, 0)
+		while True:
+			full = mine_loop(rock_locations, rock_triggers, 0)
+			if full: 
+				break
+		bank_loop(bank_locations)
+		lap += 1
 except KeyboardInterrupt:
+	print("Completed {} laps".format(lap))
 	print("Goodbye now!~")
-	bank_loop(bank_locations, rock_triggers)
 	sys.exit()
 
